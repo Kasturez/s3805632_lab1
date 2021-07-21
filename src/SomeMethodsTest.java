f2490e330b60f69b82a70b7577167dfc354782bf
kasturez <tinhuynhvinh733@gmail.com>
2021-07-21 15:35:15 +0700

    second commit

---
+++ src/SomeMethodsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeMethodsTest {
+
+    @org.junit.jupiter.api.Test
+    void weight_index() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get_weight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get_height() {
+    }
+}
